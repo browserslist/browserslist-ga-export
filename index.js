342ae864bc09a30439c4cf77393b32b2f5eca253
galvarez421 <galvarez421@gmail.com>
2020-03-01 23:04:38 -0500

    Start adding support for multiple CSVs
    - Add glob package dependency to handle finding of files
      matching glob patterns.
    - Implement generation of JSON file based on data from multiple
      report CSVs. This includes processing reportPath as glob pattern
      via glob package.
    - Rework JS to be promise-based.
    - Add headers array for clearer usage of column indices.
    - Add handleError function to represent reusable error
      handling behavior.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const csv = require('csvtojson');
 const fs = require('fs');
 const { parse } = require('browserslist-ga/src/caniuse-parser');
-const args = require('yargs').argv
+const args = require('yargs').argv;
+const glob = require('glob');
 
-const browserslistGaEntries = [];
 const defaults = {
   ignoreRows: 7,
   outputPath: 'browserslist-stats.json',
@@ -16,17 +16,23 @@ const settings = Object.assign({}, defaults, args);
 let error = false;
 
 if (typeof settings.ignoreRows !== 'number') {
-  console.error(new TypeError('browserslist-ga-export: ignoreRows must be a number.'));
+  console.error(
+    new TypeError('browserslist-ga-export: ignoreRows must be a number.')
+  );
   error = true;
 }
 
 if (typeof settings.outputPath !== 'string') {
-  console.error(new TypeError('browserslist-ga-export: outputPath must be a string.'));
+  console.error(
+    new TypeError('browserslist-ga-export: outputPath must be a string.')
+  );
   error = true;
 }
 
 if (typeof settings.reportPath !== 'string') {
-  console.error(new TypeError('browserslist-ga-export: reportPath must be a string.'));
+  console.error(
+    new TypeError('browserslist-ga-export: reportPath must be a string.')
+  );
   error = true;
 }
 
@@ -34,45 +40,85 @@ if (error === true) {
   process.exit(1);
 }
 
-csv({
-  noheader: true
-})
-  .fromFile(settings.reportPath)
-  .on('error', (err) => {
-    console.error(err);
-    process.exit(1);
-  })
-  .on('end_parsed', (rows) => {
-    // Convert row objects into row arrays expected by parse method
-    rows.forEach((row, i) => {
-      if (i >= settings.ignoreRows) {
-        const rowArray = [];
+const headers = [
+  'os',
+  'osVersion',
+  'browser',
+  'browserVersion',
+  'deviceCategory',
+  'pageViews'
+];
 
-        for (let i = 0; i <= 5; i++) {
-          rowArray[i] = row['field' + (i + 1)];
-        }
+const handleError = (err) => {
+  console.error(err);
+  process.exit(1);  
+};
 
-        // Strip commas from page views string, otherwise conversion to number in parse method will not work as expected
-        rowArray[5] = rowArray[5].split(',').join('');
-
-        // Add trailing decimal point and 0 to version number string, otherwise getSubVersion in parse method will not work as expected
-        rowArray[3] = rowArray[3] + ".0";
-
-        browserslistGaEntries.push(rowArray);
-      }
-    });
-
-    try {
-      let update = false;
-
-      if (fs.existsSync(settings.outputPath)) {
-        update = true;
-      }
-      fs.writeFileSync(settings.outputPath, JSON.stringify(parse(browserslistGaEntries), null, 2));
-      console.log(`browserslist-ga-export: ${settings.outputPath} has been ` + (update ? 'updated' : 'created') + `.`);
-      process.exit();
-    } catch (err) {
-      console.error(err);
-      process.exit(1);
-    }
+const getAllFilePaths = (pattern) => new Promise((resolve) => {
+  glob(pattern, (err, files) => {
+    resolve(files);
   });
+});
+
+const getFileRows = (path) => new Promise((resolve) => {
+  csv({
+    noheader: true
+  })
+    .fromFile(path)
+    .on('error', handleError)
+    .on('end_parsed', resolve);
+});
+
+const getAllFileRows = (paths) => Promise.all(paths.map(getFileRows));
+
+const filterAllFileRows = (allFileRows) => allFileRows
+  .map((fileRows) => fileRows.filter((row, i) => i >= settings.ignoreRows));
+
+/**
+ * Convert array of row objects returned by csvtojson into row arrays expected
+ * by browserslist-ga parse method
+ */
+const convertFileRows = (rows) => rows
+  .map((row) => headers.map((header, i) => {
+    let value = row[`field${i + 1}`];
+
+    /**
+     * Add trailing decimal point and 0 to browser version number string,
+     * otherwise getSubVersion in parse method will not work as expected
+     */
+    if (i === headers.indexOf('browserVersion')) {
+      value = `${value}.0`;
+    }
+
+    /**
+     * Strip commas from page views string, otherwise conversion to number in
+     * parse method will not work as expected
+     */
+    if (i === headers.indexOf('pageViews')) {
+      value = value.split(',').join('');
+    }
+
+    return value;
+  }));
+
+const convertAllFileRows = (allFileRows) => allFileRows.map(convertFileRows);
+
+const concatAllFileRows = (allFileRows) => allFileRows
+  .reduce((combined, fileRows) => combined.concat(fileRows));
+
+const outputJson = (rows) => {
+  try {
+    fs.writeFileSync(settings.outputPath, JSON.stringify(parse(rows), null, 2));
+    console.log(`browserslist-ga-export: ${settings.outputPath} has been ${fs.existsSync(settings.outputPath) ? 'updated' : 'created'}.`);
+    process.exit();
+  } catch (err) {
+    handleError(err);
+  }
+};
+
+getAllFilePaths(settings.reportPath)
+  .then(getAllFileRows)
+  .then(filterAllFileRows)
+  .then(convertAllFileRows)
+  .then(concatAllFileRows)
+  .then(outputJson);

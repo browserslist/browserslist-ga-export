526579e600909e56a9da71d99e3fd58dd8958ae0
galvarez421 <galvarez421@gmail.com>
2023-07-02 22:27:34 -0400

    Add support for Google Analytics 4 by default
    - Change name, description, and default value of `ignoreRows` option to
      `firstRowIndex` to improve clarity and to support Google Analytics 4
      instead of Universal Analytics by default.
    - Fix issue of rows with incorrect number of fields causing an error.
    - Update documentation.
    - Increment package version number.

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const { hideBin } = require('yargs/helpers');
 const glob = require('fast-glob');
 
 const args = yargs(hideBin(process.argv))
-  .option('ignoreRows', {
-    alias: 'i',
+  .option('firstRowIndex', {
+    alias: 'f',
     type: 'number',
-    description: 'Number of rows at beginning of report CSV file(s) to exclude from processing. Default value is based on the default format of Google Analytics custom report CSV exports.',
-    default: 7
+    description: 'Zero-based index of first data row in CSV file. Default value is based on the default format of Google Analytics 4 CSV exports.',
+    default: 8
   })
   .option('outputPath', {
     alias: 'o',
     type: 'string',
-    description: 'Output path for generated custom usage data file. If a file already exists at `--outputPath`, that file will be overwritten.',
+    description: 'Output path for generated `browserslist-stats.json` file. If a file already exists at the path, that file will be overwritten.',
     default: 'browserslist-stats.json'
   })
   .option('reportPath', {
     alias: 'r',
     type: 'string',
-    description: 'Path or [glob](https://www.npmjs.com/package/fast-glob) path pattern of report CSV file(s) to process. If a glob path pattern is provided that resolves to multiple files, the custom usage data file will represent the merged data of those files.',
+    description: 'Path or [glob](https://www.npmjs.com/package/fast-glob) path pattern of CSV file(s) to process. If a glob path pattern is provided that resolves to multiple files, the `browserslist-stats.json` file will represent the merged data of those files.',
     demandOption: true
   })
   .argv
@@ -49,7 +49,7 @@ const getFileRows = (path) =>
     noheader: true
   })
     /**
-     * Prevent empty rows from being skipped by csvtojson so that ignoreRows
+     * Prevent empty rows from being skipped by csvtojson so that firstRowIndex
      * option works as expected
      * See https://github.com/browserslist/browserslist-ga-export/issues/12
      */
@@ -64,13 +64,15 @@ const getAllFileRows = (paths) => {
 }
 
 const filterAllFileRows = (allFileRows) => allFileRows
-  .map((fileRows) => fileRows.filter((row, i) => i >= args.ignoreRows));
+  .map((fileRows) => fileRows.filter((row, i) => i >= args.firstRowIndex));
 
 /**
  * Convert array of row objects returned by csvtojson into row arrays expected
  * by browserslist-ga parse method
  */
 const convertFileRows = (rows) => rows
+  // Ignore rows with incorrect number of fields
+  .filter((row) => Object.keys(row).length === headers.length)
   .map((row) => headers.map((header, i) => {
     let value = row[`field${i + 1}`];
 
